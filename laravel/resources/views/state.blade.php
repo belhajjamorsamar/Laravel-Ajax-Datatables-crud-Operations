54470eb9d1af9c6c393edf3367a6673a43c68a22
Samar bel hajj amor <belhajjamorsamar@gmail.com>
2023-12-07 13:48:47 +0100

    fix forum

--- laravel/resources/views/state.blade.php
+++ laravel/resources/views/state.blade.php
@@ -4,58 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <title>Laravel Ajax </title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
+
+    <title>Document</title>
 </head>
 <body>
-    <div class="container">
-<h1 class="text-center">Laravel Ajax Crud Operations </h1>
-<hr>
-<div class="row mt-5">
-    <div class="col-6 offset-3">
-<form action="" id="myForm">
-    <div class="form-group">
-        <label for="">Select State</label>
-        <select name="" id="" class="form-control">
-        @foreach($states as $state)
-        <option value="{{ $state->id }}">{{ $state->state_name }}</option>
-        @endforeach
-        </select>
+
+<div class="container">
+    <h1 class="text-center">Laravel Ajax </h1>
+    <div class="row">
+        <div class="col">
+           <form>
+            <div class="form-group">
+                <label for="">Select state</label>
+                <select name="state"  class="form-control">
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="">City Name</label>
+                <input type="text" name="city_name" class="form-control">
+
+            </div>
+
+            <button class="btn btn-success">Add city</button>
+           </form>
+        </div>
     </div>
-    <div class="form-group">
-        <label for="">City Name</label>
-        <input type="text" name="city_name" class="form-control"></div>
-
-    <button class="btn btn-success" id="submit">Add city</button>
-
-</form>
-    </div>
-</div>
-<hr>
-
-</div>
-</div>
-</div>
-<script src="https://code.jquery.com/jquery-3.7.0.js"></script>
-<script>
-    $(document).ready(function(){
-        $('#submit').click(function(e) {
-            e.preventDefault();
-            $.ajax({
-                url:"",
-                type:"post",
-                dataType :"json",
-                data:('#myForm').serialize(),
-                success:function(response){
-                    console.log(response);
-                }
-
-
-            });
-
-        })
-    });
-</script>
-
+</div>  
 </body>
 </html>
